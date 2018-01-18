b6e907a35920fdc25df792b0cb560b486d7599c4
Alex Sanchez <alesanchez728@gmail.com>
2018-01-17 21:08:54 -0600

    works

---
+++ LetterLoops/Process.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetterLoops
+{
+    class Process
+    {
+        static public void ConvertInput(string userInput)
+        {
+            int index = 1;
+
+            foreach (var letter in userInput)
+            {
+                string currentLetter = letter.ToString();
+                for (int i = 1; i <= index; i++)
+                {
+                    if (i == 1 && index != 1)
+                    {
+                        var uppercaseCurrentLetter = currentLetter.ToUpper();
+                        Console.Write($"-{uppercaseCurrentLetter}");
+                    }
+                    else if (i == 1 && index == 1)
+                    {
+                        var uppercaseCurrentLetter = currentLetter.ToUpper();
+                        Console.Write($"{uppercaseCurrentLetter}");
+                    }
+                    else
+                    {
+                        currentLetter = currentLetter.ToLower();
+                        Console.Write($"{currentLetter}");
+                    }
+                }
+                index++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LetterLoops
 {
@@ -10,6 +7,8 @@ namespace LetterLoops
     {
         static void Main(string[] args)
         {
+            var userInput = Console.ReadLine();
+            Process.ConvertInput(userInput);
         }
     }
 }
